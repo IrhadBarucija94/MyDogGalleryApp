b96c404b5c269494834efb2a58deb91ac3cf9689
Irhad <selmajasarevic.selma@gmail.com>
2023-09-29 23:20:13 +0200

    Initial Commit

---
+++ MyDogsPictures/MyDogsPicturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDogsPicturesApp.swift
+//  MyDogsPictures
+//
+//  Created by Irhad Baručija on 29. 9. 2023..
+//
+
+import SwiftUI
+
+@main
+struct MyDogsPicturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
